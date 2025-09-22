adfaf808e34ccd3c6a20f49a4dfbb3021211db92
Iliya Kushnir <iliyakyshnir07@gmail.com>
2025-09-22 21:26:03 +0300

    small fixes deploy 3

--- shop/src/app/search-for/page.tsx
+++ shop/src/app/search-for/page.tsx
@@ -1,12 +1,17 @@
-"use client"
-import SearchingFeed from "@/components/SearchingField/SearchingField"
+// app/search-for/page.tsx
+"use client";
 
+import dynamic from "next/dynamic";
+
+const SearchingFeedNoSSR = dynamic(
+  () => import("@/components/SearchingField/SearchingField"),
+  { ssr: false }
+);
 
 export default function Search() {
-
-    return(
-        <div className="font-sans flex flex-col items-center justify-items-center  p-2.5 pb-2.5 pt-[75px] sm:p-20">
-            <SearchingFeed />
-        </div>
-    )
+  return (
+    <div className="font-sans flex flex-col items-center justify-items-center p-2.5 pb-2.5 pt-[75px] sm:p-20">
+      <SearchingFeedNoSSR />
+    </div>
+  );
 }
